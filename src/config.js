851808e20fd7168b5b3e059a7d50cbf403040346
paulgoblin <mpr75@cornell.edu>
2018-09-23 22:15:40 -0700

    Faster

--- src/config.js
+++ src/config.js
@@ -5,13 +5,13 @@ function config(root) {
   }
 
   const fieldOfView= {
-    range: 100,
+    range: 120,
     // 120  degree field of view at 1200px screen width
     tanTheta: (viewport.width / 1200) * Math.tan( (120 / 2) / 180 * Math.PI)
   }
 
   return {
-    speed: 0.3,
+    speed: 0.5,
     rot: 2 / 180 * Math.PI,
     framerate: Math.floor(1000 / 60), // 60 fps
     root,
